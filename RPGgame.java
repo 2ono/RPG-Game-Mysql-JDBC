39e52fef76d650aed19b87929d8dc24f599e790a
2ono <clt3555@gmail.com>
2023-05-23 16:08:39 +0900

    [add] create characters

--- RPGgame.java
+++ RPGgame.java
@@ -1,7 +1,39 @@
+import java.sql.SQLException;
+import java.util.Scanner;
 
 public class RPGgame {
 
-	public static void main(String[] args) {
-		System.out.println("Hello all");
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		
+		
+	
+		while (true) {
+			Scanner scan = new Scanner(System.in);
+			System.out.println("Welcome to RPG game");
+			System.out.println("Press 1 to enroll Character");
+			System.out.println("Press 2 to check status Character");
+			System.out.println("Press 3 to hunt animals");
+			int num = scan.nextInt();
+			RPGgameDAO dao = new RPGgameDAO();
+			
+			
+			switch (num) {
+			case 1:
+//				int id, String name, int age, String email, String address
+				System.out.println("Submit your nickname");
+				String name = scan.next();
+				dao.enrollGame(name);
+				
+				System.out.println("Congrat! you did done");
+				break;
+			case 2:
+				System.out.println("Check your status");
+			case 3:
+				System.out.println("Hunting animal!");
+				
+				
+			}
+			
+		}
 	}
 }
--- RPGgameDAO.java
+++ RPGgameDAO.java
@@ -1,18 +1,44 @@
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class RPGgameDAO {
 
-	public void enrollGame() throws ClassNotFoundException, SQLException {
-		Connection conn = new JDBC().createC();
-		
-		String q = "insert into human(id,name,age,email,address) values(?,?,?,?,?);";
-		
+	public void enrollGame(String name)
+			throws SQLException {
+		try {
+
+			Connection c = JDBC.createC();
+//			String q = "insert into human(id,name,age,email,address) values(?,?,?,?,?);";
+			String q = "insert into human(human_name) values(?)";
+
+			// 자동커밋 비활성화
+			c.setAutoCommit(false);
+			// sql injection 공격 예방
+			PreparedStatement pstm = c.prepareStatement(q);
+
+//			pstm.setInt(1, id);
+			pstm.setString(1, name);
+//			pstm.setInt(3, age);
+//			pstm.setString(4, email);
+//			pstm.setString(5, address);
+			pstm.addBatch();
+			// 배치 실행
+			pstm.executeBatch();
+			// 커밋
+			c.commit();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 		// stop autu commit
 //		conn.getAutoCommit(false);
 		// 1, 'james',30,'sky@gmail.com','south korea country soul city'
-		
+
 		// visit restaurants to gain Level resrant table > level table > human table
-		
+
 	}
 }
